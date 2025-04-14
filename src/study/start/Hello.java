113cfee96f57609c187a7b37184eb481c6c29946
sycker <wisdbsk21@naver.com>
2025-04-14 12:41:30 +0900

    java_study

---
+++ src/study/start/Hello.java
@@ -0,0 +1,24 @@
+package study.start;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World");
+		
+		// 주석
+		//실행(Run) : ctrl + F11
+		
+		//주석
+		단축키: Ctrl + //
+			 ctrl + shift + c
+		 
+			  /*
+//			   * 여러줄 주석
+//			   *
+			   */
+			  
+	}
+
+}
